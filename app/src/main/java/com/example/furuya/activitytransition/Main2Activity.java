bcb04e12c83723b653d8d620417fbfc92571ccc9
gouf <innocent.zero@gmail.com>
2016-11-29 07:21:06 +0900

    アクティビティ移動後に初期アクティビティに移動するボタンを追加

--- app/src/main/java/com/example/furuya/activitytransition/Main2Activity.java
+++ app/src/main/java/com/example/furuya/activitytransition/Main2Activity.java
@@ -1,13 +1,25 @@
 package com.example.furuya.activitytransition;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Main2Activity extends AppCompatActivity {
+  Context context;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    context = Main2Activity.this;
     setContentView(R.layout.activity_main2);
+  }
+
+  public void transit(View v) {
+    Intent intent = new Intent(context, MainActivity.class);
+    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+    context.startActivity(intent);
+    overridePendingTransition(0, 0);
   }
 }
