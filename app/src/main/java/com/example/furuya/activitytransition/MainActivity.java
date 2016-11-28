7a391fbd0fbca505c54c63ebe7d65e74ab6ef131
gouf <innocent.zero@gmail.com>
2016-11-29 07:19:59 +0900

    ボタンクリックによるアクティビティの移動処理を追加

--- app/src/main/java/com/example/furuya/activitytransition/MainActivity.java
+++ app/src/main/java/com/example/furuya/activitytransition/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.furuya.activitytransition;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+  Context context;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    context = MainActivity.this;
     setContentView(R.layout.activity_main);
+  }
+
+  public void transit(View v) {
+    Intent intent = new Intent(context, Main2Activity.class);
+    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+    context.startActivity(intent);
+    overridePendingTransition(0, 0);
   }
 }
